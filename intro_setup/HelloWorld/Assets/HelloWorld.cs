e5b8927e2fd26f97d3edb09697031a8459df6ea2
ridizarri <ridizarri@hexacta.com>
2018-12-17 16:38:06 -0300

    Initial commit

---
+++ intro_setup/HelloWorld/Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        print("Hello Ramiro y Octavio!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
